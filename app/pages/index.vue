ac08f3166de6054ed85282eb298ec5a8898ada20
Barberr <kachxxx@gmail.com>
2026-01-16 17:42:27 +0300

    Chg main page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -74,7 +74,7 @@
 
                 <!-- Карточки товаров -->
                 <div
-                    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6"
+                    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-6"
                 >
                     <ProductCard
                         v-for="product in sortedProducts"
