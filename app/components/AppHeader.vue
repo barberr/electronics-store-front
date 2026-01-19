8943580a79c0fae4e124369603f387b5a5a6851d
Barberr <kachxxx@gmail.com>
2026-01-19 19:09:51 +0300

    Chk menu katalog width

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -179,7 +179,7 @@
                                 <!-- Контент -->
                                 <div
                                     v-else
-                                    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-2"
+                                    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2"
                                 >
                                     <div
                                         v-for="group in data?.catalog || []"
