c4f1c3cf9dcdd9dbcf6f85f4272f422b0193b216
Barberr <kachxxx@gmail.com>
2026-01-20 16:10:48 +0300

    Chk heroicons2

--- app/app.config.ts
+++ app/app.config.ts
@@ -19,5 +19,18 @@ export default defineAppConfig({
                 'sans-serif',
             ],
         },
+        icons: {
+            // Указываем только нужные иконки
+            heroicons: {
+                photo: 'i-heroicons-photo',
+                'shopping-bag': 'i-heroicons-shopping-bag',
+                'arrow-down': 'i-heroicons-arrow-down',
+                home: 'i-heroicons-home',
+                user: 'i-heroicons-user',
+                cog: 'i-heroicons-cog',
+                bell: 'i-heroicons-bell',
+                'magnifying-glass': 'i-heroicons-magnifying-glass',
+            },
+        },
     },
 });
