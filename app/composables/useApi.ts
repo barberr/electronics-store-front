59908747fac460fb3fdbb75a09171a4a7adba655
Barberr <kachxxx@gmail.com>
2026-02-14 13:33:37 +0300

    feat: use different endpoints for dev and prod

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -7,12 +7,15 @@ declare module 'axios' {
         skipAuth?: boolean;
     }
 }
-
+const baseURL = process.env.NODE_ENV === 'production' 
+  ? 'https://izistore.info/api/'
+  : 'http://127.0.0.1:8000/api/'
 const api = axios.create({
     // baseURL: 'http://127.0.0.1:8000/api/',
     // baseURL: 'http://10.10.4.12:8000/api/', // ← твой DRF API
     // baseURL: 'https://94.241.171.81/api/',
-    baseURL: 'https://izistore.info/api/',
+    // baseURL: 'https://izistore.info/api/',
+    baseURL,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
